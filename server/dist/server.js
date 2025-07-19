7d3700d7e6f315e46d40e1b46afb7a3fe0d03444
Abdurahman <eabdurahman8@gmail.com>
2025-07-19 16:54:27 +0300

    prisma connect with mongodb

--- server/dist/server.js
+++ server/dist/server.js
@@ -3,8 +3,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+require("dotenv/config");
 const app_1 = __importDefault(require("./app"));
 const PORT = process.env.PORT || 3000;
-app_1.default.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+const mongoURI = process.env.DATABASE_URL || "";
+// Replace with your MongoDB URI
+mongoose_1.default.connect(mongoURI)
+    .then(() => {
+    console.log('✅ MongoDB connected successfully!');
+    // Start server only after DB is connected
+    app_1.default.listen(PORT, () => {
+        console.log(`🚀 Server is running on http://localhost:${PORT}`);
+    });
+})
+    .catch(err => {
+    console.error('❌ MongoDB connection error:', err);
 });
--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,24 @@
 
+
+import mongoose from "mongoose";
+import 'dotenv/config'
 import app from "./app";
 
 const PORT = process.env.PORT || 3000;
+const mongoURI = process.env.DATABASE_URL || "";
+// Replace with your MongoDB URI
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+
+mongoose.connect(mongoURI)
+.then(() => {
+  console.log('✅ MongoDB connected successfully!');
+
+  // Start server only after DB is connected
+  app.listen(PORT, () => {
+    console.log(`🚀 Server is running on http://localhost:${PORT}`);
+  });
+
+})
+.catch(err => {
+  console.error('❌ MongoDB connection error:', err);
 });
