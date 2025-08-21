ead8ed425fe4866ac67650afd86a2b2efd88c8de
Abdurahman <eabdurahman8@gmail.com>
2025-08-21 15:26:52 +0300

    all application related APIS

--- server/routes/application.route.ts
+++ server/routes/application.route.ts
@@ -3,14 +3,15 @@ import upload from "../config/multer";
 
 
 import { verifyCompanyAccess, verifyInternAccess } from "../middlewares/auth.middleware";
-import {applyForJob, preCheckJob, updateStatus } from "../controllers/application.controller";
+import {applyForJob, getAllApplication, preCheckJob, updateStatus } from "../controllers/application.controller";
+import { verify } from "crypto";
 
 const application = Router();
 
 application.post("/:jobId/apply",upload.single("resume"),applyForJob);
-// application.put("/update/status",updateStatus);
-// application.get("/checkJob",preCheckJob);
-// application.get("/getCampantApplication",preCheckJob);
+application.put("/update/:applicationId/status",verifyCompanyAccess,updateStatus);
+application.get("/:applicationId",verifyCompanyAccess,getAllApplication);
+application.get("/precheck/:jobId",preCheckJob);
 
 
 
